b0766a9c987439c5e97ff500c25aa468719b096d
Shawn VanderJagt <shawnvanderjagt@navapbc.com>
2022-06-23 14:04:53 -0500

    initial

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,8 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp1({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default MyApp1
